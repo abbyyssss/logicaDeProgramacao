957c01ad77e1fab08c11921123883a81e7b673ac
Kauan <kauan.mcvquaresma@senacsp.edu.br>
2024-08-01 13:39:09 -0300

    calculadora de áreas

---
+++ logicaDeProgramacao/forms/calculadora areas/calculadora areas/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace calculadora_areas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblinicio = new Label();
+            RadioQuadrado = new RadioButton();
+            RadioTriangulo = new RadioButton();
+            RadioRetangulo = new RadioButton();
+            RadioCirculo = new RadioButton();
+            RadioTrapezio = new RadioButton();
+            lblPergunta = new Label();
+            pictureBox1 = new PictureBox();
+            lblFormula = new Label();
+            lblsla = new Label();
+            btnlimpa = new Button();
+            btnfazer = new Button();
+            btnb1 = new TextBox();
+            btnb2 = new TextBox();
+            btnb3 = new TextBox();
+            lblf1 = new Label();
+            lblf2 = new Label();
+            lblf3 = new Label();
+            lblex = new Label();
+            lblex2 = new Label();
+            lblexp = new Label();
+            lblresult = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblinicio
+            // 
+            lblinicio.AutoSize = true;
+            lblinicio.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblinicio.Location = new Point(12, 9);
+            lblinicio.Name = "lblinicio";
+            lblinicio.Size = new Size(270, 30);
+            lblinicio.TabIndex = 0;
+            lblinicio.Text = "CALCULADORA DE ÁREAS";
+            // 
+            // RadioQuadrado
+            // 
+            RadioQuadrado.AutoSize = true;
+            RadioQuadrado.Location = new Point(20, 94);
+            RadioQuadrado.Name = "RadioQuadrado";
+            RadioQuadrado.Size = new Size(78, 19);
+            RadioQuadrado.TabIndex = 1;
+            RadioQuadrado.TabStop = true;
+            RadioQuadrado.Text = "Quadrado";
+            RadioQuadrado.UseVisualStyleBackColor = true;
+            RadioQuadrado.CheckedChanged += RadioQuadrado_CheckedChanged;
+            // 
+            // RadioTriangulo
+            // 
+            RadioTriangulo.AutoSize = true;
+            RadioTriangulo.Location = new Point(20, 129);
+            RadioTriangulo.Name = "RadioTriangulo";
+            RadioTriangulo.Size = new Size(73, 19);
+            RadioTriangulo.TabIndex = 2;
+            RadioTriangulo.TabStop = true;
+            RadioTriangulo.Text = "triângulo";
+            RadioTriangulo.UseVisualStyleBackColor = true;
+            RadioTriangulo.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // RadioRetangulo
+            // 
+            RadioRetangulo.AutoSize = true;
+            RadioRetangulo.Location = new Point(19, 164);
+            RadioRetangulo.Name = "RadioRetangulo";
+            RadioRetangulo.Size = new Size(79, 19);
+            RadioRetangulo.TabIndex = 3;
+            RadioRetangulo.TabStop = true;
+            RadioRetangulo.Text = "Retângulo";
+            RadioRetangulo.UseVisualStyleBackColor = true;
+            RadioRetangulo.CheckedChanged += RadioRetangulo_CheckedChanged;
+            // 
+            // RadioCirculo
+            // 
+            RadioCirculo.AutoSize = true;
+            RadioCirculo.Location = new Point(20, 201);
+            RadioCirculo.Name = "RadioCirculo";
+            RadioCirculo.Size = new Size(63, 19);
+            RadioCirculo.TabIndex = 4;
+            RadioCirculo.TabStop = true;
+            RadioCirculo.Text = "Circulo";
+            RadioCirculo.UseVisualStyleBackColor = true;
+            RadioCirculo.CheckedChanged += RadioCirculo_CheckedChanged;
+            // 
+            // RadioTrapezio
+            // 
+            RadioTrapezio.AutoSize = true;
+            RadioTrapezio.Location = new Point(20, 241);
+            RadioTrapezio.Name = "RadioTrapezio";
+            RadioTrapezio.Size = new Size(68, 19);
+            RadioTrapezio.TabIndex = 5;
+            RadioTrapezio.TabStop = true;
+            RadioTrapezio.Text = "Trapézio";
+            RadioTrapezio.UseVisualStyleBackColor = true;
+            RadioTrapezio.CheckedChanged += RadioTrapezio_CheckedChanged;
+            // 
+            // lblPergunta
+            // 
+            lblPergunta.AutoSize = true;
+            lblPergunta.Location = new Point(19, 39);
+            lblPergunta.Name = "lblPergunta";
+            lblPergunta.Size = new Size(168, 15);
+            lblPergunta.TabIndex = 6;
+            lblPergunta.Text = "qual área você deseja calcular?\r\n";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(246, 48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(222, 212);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // lblFormula
+            // 
+            lblFormula.AutoSize = true;
+            lblFormula.Location = new Point(337, 277);
+            lblFormula.Name = "lblFormula";
+            lblFormula.Size = new Size(0, 15);
+            lblFormula.TabIndex = 8;
+            lblFormula.Click += lblFormula_Click;
+            // 
+            // lblsla
+            // 
+            lblsla.AutoSize = true;
+            lblsla.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblsla.Location = new Point(242, 272);
+            lblsla.Name = "lblsla";
+            lblsla.Size = new Size(90, 21);
+            lblsla.TabIndex = 9;
+            lblsla.Text = "FÓRMULA:";
+            // 
+            // btnlimpa
+            // 
+            btnlimpa.Location = new Point(31, 513);
+            btnlimpa.Name = "btnlimpa";
+            btnlimpa.Size = new Size(97, 42);
+            btnlimpa.TabIndex = 10;
+            btnlimpa.Text = "limpa";
+            btnlimpa.UseVisualStyleBackColor = true;
+            // 
+            // btnfazer
+            // 
+            btnfazer.Location = new Point(352, 513);
+            btnfazer.Name = "btnfazer";
+            btnfazer.Size = new Size(98, 42);
+            btnfazer.TabIndex = 11;
+            btnfazer.Text = "resolver";
+            btnfazer.UseVisualStyleBackColor = true;
+            btnfazer.Click += btnfazer_Click;
+            // 
+            // btnb1
+            // 
+            btnb1.Location = new Point(31, 369);
+            btnb1.Name = "btnb1";
+            btnb1.Size = new Size(100, 23);
+            btnb1.TabIndex = 12;
+            btnb1.Visible = false;
+            // 
+            // btnb2
+            // 
+            btnb2.Location = new Point(195, 369);
+            btnb2.Name = "btnb2";
+            btnb2.Size = new Size(100, 23);
+            btnb2.TabIndex = 13;
+            // 
+            // btnb3
+            // 
+            btnb3.Location = new Point(350, 369);
+            btnb3.Name = "btnb3";
+            btnb3.Size = new Size(100, 23);
+            btnb3.TabIndex = 14;
+            btnb3.Visible = false;
+            // 
+            // lblf1
+            // 
+            lblf1.AutoSize = true;
+            lblf1.Location = new Point(60, 340);
+            lblf1.Name = "lblf1";
+            lblf1.Size = new Size(0, 15);
+            lblf1.TabIndex = 15;
+            // 
+            // lblf2
+            // 
+            lblf2.AutoSize = true;
+            lblf2.Location = new Point(226, 340);
+            lblf2.Name = "lblf2";
+            lblf2.Size = new Size(0, 15);
+            lblf2.TabIndex = 16;
+            // 
+            // lblf3
+            // 
+            lblf3.AutoSize = true;
+            lblf3.Location = new Point(386, 340);
+            lblf3.Name = "lblf3";
+            lblf3.Size = new Size(0, 15);
+            lblf3.TabIndex = 17;
+            // 
+            // lblex
+            // 
+            lblex.AutoSize = true;
+            lblex.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblex.Location = new Point(42, 419);
+            lblex.Name = "lblex";
+            lblex.Size = new Size(86, 21);
+            lblex.TabIndex = 18;
+            lblex.Text = "expressão";
+            // 
+            // lblex2
+            // 
+            lblex2.AutoSize = true;
+            lblex2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblex2.Location = new Point(370, 419);
+            lblex2.Name = "lblex2";
+            lblex2.Size = new Size(82, 21);
+            lblex2.TabIndex = 19;
+            lblex2.Text = "resultado";
+            // 
+            // lblexp
+            // 
+            lblexp.AutoSize = true;
+            lblexp.Location = new Point(60, 459);
+            lblexp.Name = "lblexp";
+            lblexp.Size = new Size(0, 15);
+            lblexp.TabIndex = 20;
+            // 
+            // lblresult
+            // 
+            lblresult.AutoSize = true;
+            lblresult.Location = new Point(398, 459);
+            lblresult.Name = "lblresult";
+            lblresult.Size = new Size(0, 15);
+            lblresult.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(511, 567);
+            Controls.Add(lblresult);
+            Controls.Add(lblexp);
+            Controls.Add(lblex2);
+            Controls.Add(lblex);
+            Controls.Add(lblf3);
+            Controls.Add(lblf2);
+            Controls.Add(lblf1);
+            Controls.Add(btnb3);
+            Controls.Add(btnb2);
+            Controls.Add(btnb1);
+            Controls.Add(btnfazer);
+            Controls.Add(btnlimpa);
+            Controls.Add(lblsla);
+            Controls.Add(lblFormula);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblPergunta);
+            Controls.Add(RadioTrapezio);
+            Controls.Add(RadioCirculo);
+            Controls.Add(RadioRetangulo);
+            Controls.Add(RadioTriangulo);
+            Controls.Add(RadioQuadrado);
+            Controls.Add(lblinicio);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblinicio;
+        private RadioButton RadioQuadrado;
+        private RadioButton RadioTriangulo;
+        private RadioButton RadioRetangulo;
+        private RadioButton RadioCirculo;
+        private RadioButton RadioTrapezio;
+        private Label lblPergunta;
+        private PictureBox pictureBox1;
+        private Label lblFormula;
+        private Label lblsla;
+        private Button btnlimpa;
+        private Button btnfazer;
+        private TextBox btnb1;
+        private TextBox btnb2;
+        private TextBox btnb3;
+        private Label lblf1;
+        private Label lblf2;
+        private Label lblf3;
+        private Label lblex;
+        private Label lblex2;
+        private Label lblexp;
+        private Label lblresult;
+    }
+}
---
+++ logicaDeProgramacao/forms/calculadora areas/calculadora areas/Form1.cs
@@ -0,0 +1,161 @@
+﻿namespace calculadora_areas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioTriangulo.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\kauan.mcvquaresma\Desktop\hoje\triangulo-5_xl.jpeg");
+                lblFormula.Text = "b*h/2";
+                lblf1.Text = " lado ";
+                lblf2.Text = " altura ";
+                btnb2.Visible = true;
+                btnb1.Visible = true;
+                btnb3.Visible = false;
+                lblf3.Text = "";
+                btnb1.Focus();
+            }
+        }
+
+        private void RadioQuadrado_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioQuadrado.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\kauan.mcvquaresma\Desktop\hoje\quadrado.jfif");
+                lblFormula.Text = "l²";
+                lblf2.Text = " lado ";
+                btnb2.Visible = true;
+                lblf1.Text = "";
+                lblf3.Text = "";
+                btnb1.Visible = false;
+                btnb3.Visible = false;
+                btnb2.Focus();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioRetangulo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioRetangulo.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\kauan.mcvquaresma\Desktop\hoje\retangulo-roxo-claro.jpg");
+                lblFormula.Text = "b*h";
+                lblf3.Text = "";
+                lblf1.Text = " base ";
+                lblf2.Text = " altura ";
+                btnb2.Visible = true;
+                btnb1.Visible = true;
+                btnb3.Visible = false;
+                btnb1.Focus();
+            }
+        }
+
+        private void RadioCirculo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioCirculo.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\kauan.mcvquaresma\Desktop\hoje\250px-Circle_-_black_simple.svg.png");
+                lblFormula.Text = "πr²";
+                lblf1.Text = "";
+                lblf3.Text = "";
+                lblf2.Text = " raio ";
+                btnb1.Visible = false;
+                btnb2.Visible = true;
+                btnb3.Visible = false;
+                btnb2.Focus();
+            }
+        }
+
+        private void RadioTrapezio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioTrapezio.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\kauan.mcvquaresma\Desktop\hoje\download.png");
+                lblFormula.Text = "((B+b)*h/2)";
+                lblf1.Text = " base (B)";
+                lblf2.Text = " base (b) ";
+                lblf3.Text = " altura ";
+                btnb2.Visible = true;
+                btnb1.Visible = true;
+                btnb3.Visible = true;
+                btnb1.Focus();
+            }
+        }
+
+        private void lblFormula_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnfazer_Click(object sender, EventArgs e)
+        {
+            double ladoum = 0;
+            double ladodois = 0;
+            double seila = 0;
+            double resultado;
+
+            if (RadioTrapezio.Checked)
+            {
+                ladoum = double.Parse(btnb1.Text); // base maior
+                ladodois = double.Parse(btnb2.Text); // base menor
+                seila = double.Parse(btnb3.Text); // altura
+                resultado = (ladoum + ladodois) * seila / 2;
+                lblresult.Text = resultado.ToString();
+                lblexp.Text = $"(({ladoum} + {ladodois}) * {seila}) / 2";
+                btnb1.Focus();
+            }
+            else if (RadioTriangulo.Checked)
+            {
+                ladoum = double.Parse(btnb1.Text); // base
+                ladodois = double.Parse(btnb2.Text); // altura
+                resultado = (ladoum * ladodois) / 2;
+                lblresult.Text = resultado.ToString();
+                lblexp.Text = $"{ladoum} * {ladodois} / 2";
+                btnb1.Focus();
+            }
+            else if (RadioQuadrado.Checked)
+            {
+                ladoum = double.Parse(btnb2.Text); // lado
+                resultado = ladoum * ladoum;
+                lblresult.Text = resultado.ToString();
+                lblexp.Text = $"{ladoum}²";
+                btnb2.Focus();
+            }
+            else if (RadioRetangulo.Checked)
+            {
+                ladoum = double.Parse(btnb1.Text); // base
+                ladodois = double.Parse(btnb2.Text); // altura
+                resultado = ladoum * ladodois;
+                lblresult.Text = resultado.ToString();
+                lblexp.Text = $"{ladoum} * {ladodois}";
+                btnb1.Focus();
+            }
+            else if (RadioCirculo.Checked)
+            {
+                ladodois = double.Parse(btnb2.Text); // raio
+                resultado = Math.PI * ladodois * ladodois;
+                lblresult.Text = Math.Round(resultado, 2).ToString(); // Arredonda o resultado para 2 casas decimais
+                lblexp.Text = $"π * {ladodois}²";
+                btnb2.Focus();
+            }
+            btnb1.Text = "";
+            btnb2.Text = "";
+            btnb3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
